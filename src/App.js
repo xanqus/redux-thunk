b2692eac189ae7d333ded26d2974a48febd27db8
xanqus <sbk98031816@gmail.com>
2021-04-06 19:04:04 +0900

    redux-thunk

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
+
+function App() {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+}
+
+export default App;
